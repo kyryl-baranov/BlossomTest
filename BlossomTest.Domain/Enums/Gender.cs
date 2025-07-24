b26536da4a376d85ba1be63ac8eda617ce2edbc6
Kyryl <baranov.k.a0@gmail.com>
2025-07-25 01:01:55 +0300

    Move test projects to tests folder

--- BlossomTest.Domain/Enums/Gender.cs
+++ BlossomTest.Domain/Enums/Gender.cs
@@ -1,17 +1,7 @@
 ﻿namespace BlossomTest.Domain.Enums;
 
-/// <summary>
-/// Represents the gender of a user.
-/// </summary>
 public enum Gender
-{
-    /// <summary>
-    /// Represents a male gender.
-    /// </summary>
+{    
     Male,
-
-    /// <summary>
-    /// Represents a female gender.
-    /// </summary>
     Female
 }
